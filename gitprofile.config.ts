4a4050ab383c41b4921b783eeb0434435f9799a6
Adebayo Adetunji <adetunji003@gmail.com>
2026-04-27 10:02:47 -0400

    Update gitprofile.config.ts

--- gitprofile.config.ts
+++ gitprofile.config.ts
@@ -104,7 +104,7 @@ const CONFIG = {
   },
   resume: {
     fileUrl:
-      'https://drive.google.com/file/d/1QiIZs3wWQCkkI_RabS2VpkTwO0pxwl24/view?usp=drive_link', // Empty fileUrl will hide the `Download Resume` button.
+      'https://drive.google.com/file/d/1h9tYlSWFV6swRilCaJ26y5mfHJdFC8oq/view?usp=drive_link', // Empty fileUrl will hide the `Download Resume` button.
   },
   skills: [
     'React',
